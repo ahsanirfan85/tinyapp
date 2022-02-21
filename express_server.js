7b8de09e36339e5923ea1c11adc9ae2a0d8397ad
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-02-21 15:24:02 +0000

    updated code to use ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080 - changed to 3000 because 8080 wasn't working on my computer
 
+app.set("view enginer", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
