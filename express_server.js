3fd1fad4704407a05ae97591748c464598c44f8b
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-02-21 14:27:04 +0000

    setup an express server

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3000; // default port 8080 - changed to 3000 because 8080 wasn't working on my computer
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
